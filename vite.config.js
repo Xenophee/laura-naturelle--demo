3c92e6c869f9da56fe298466a6521be6706903a7
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-03 08:21:17 +0200

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default {
     build: {
         rollupOptions: {
             input: {
+                config: resolve(__dirname, 'vercel.json'),
                 main: resolve(__dirname, 'index.html'),
                 pages: resolve(__dirname, 'pages/page.html')
             }

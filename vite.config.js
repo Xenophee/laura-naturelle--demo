9ac0b1e5ed295d27fd45394f923e3d67d23f89d1
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-06 11:45:16 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default {
                 config: resolve(__dirname, 'vercel.json'),
                 main: resolve(__dirname, 'index.html'),
                 pages: resolve(__dirname, 'pages/page.html'),
-                female: resolve(__dirname, 'pages/female.html')
+                female: resolve(__dirname, 'pages/female.html'),
+                children: resolve(__dirname, 'pages/children.html')
             }
         }
     }

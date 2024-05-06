e628ce0fbf03008ff6d3546d02484c55e136186c
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-06 18:08:58 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
+
 import { resolve } from 'path';
+
 
 export default {
     build: {
@@ -9,7 +11,11 @@ export default {
                 pages: resolve(__dirname, 'pages/page.html'),
                 female: resolve(__dirname, 'pages/female.html'),
                 male: resolve(__dirname, 'pages/male.html'),
-                children: resolve(__dirname, 'pages/children.html')
+                children: resolve(__dirname, 'pages/children.html'),
+                service: resolve(__dirname, 'pages/service.html'),
+                giftCard: resolve(__dirname, 'pages/gift_card.html'),
+                products: resolve(__dirname, 'pages/products.html'),
+                contact: resolve(__dirname, 'pages/contact.html'),
             }
         }
     }
